1dc93a73a4735f6fa0c00dcff47ec0848bf487de
Emmanuel Oklo Brown <emmanuelbrown1310@gmail.com>
2019-09-06 07:02:49 -0400

    Made site swipeable

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,26 +6,59 @@ import Footer from '../Components/Footer/Footer'
 import Scroll from '../Components/Home/scroll'
 import Bar from '../Components/GenUtils/Bar'
 import '../Components/Home/home.scss'
+import { useSwipeable, Swipeable } from 'react-swipeable'
 
-export default class Home extends React.Component{
+
+class Home extends React.Component{
     constructor(props){
         super(props)
         this.state = {
-            input: "",
+            position: 0,
         }
     }
+
+    mover = (swipedUp) =>{
+        let { position } = this.state
+        if(swipedUp){
+            position += 1
+        } else{
+            position -= 1
+        }
+        console.log(position)
+        this.setState({ position })
+    }
+
     render(){
+        // const handlers = useSwipeable({
+        //     onSwipedUp: () => window.location.href = "#footer",
+        //     preventDefaultTouchmoveEvent: true,
+        //     trackMouse: true
+        //   });
+        // const working = {
+        //     onSwipedUp: () => console.log("going up"),
+        //     onSwipedDown: () => console.log("going down")
+        // }
+        const SwipeStyle = {
+            transform: `translate3d(0px, -${this.state.position}00vh, 0px)`, 
+            transition: '1s ease all'
+        }
         return(
-            <div>
-                <Navbar />
+            <Swipeable 
+            onSwipedUp={() => this.mover(true)} 
+            onSwipedDown={() => this.mover(false)} 
+            style={SwipeStyle}>
                 <section id="firstView">
+                    <Navbar />
                     <Headshot />
-                    <Scroll id="thingy"/>
+                    <Scroll />
                 </section>
-                <Bar isTop={true} onClick={this.clicker} to="#title"/>
+                <Bar isTop={true} to="#firstView"/>
                 <Footer />
                 <Bar isTop={false}/>
-            </div>
+            </Swipeable>
+        
         )
     }
 }
+
+export default Home
