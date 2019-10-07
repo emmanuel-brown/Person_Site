d5fc12d62be5a0ef4ac08414648b99d5176bb4a8
Emmanuel Oklo Brown <emmanuelbrown1310@gmail.com>
2019-10-07 11:01:09 -0400

    Added position

--- src/Components/Home/Headshot.jsx
+++ src/Components/Home/Headshot.jsx
@@ -4,7 +4,12 @@ import HeadShot from './headShot1.jpeg'
 const Headshot = () =>{
     return(
         <>
+        <div id="headShot">
             <img src={ HeadShot } alt=""/>
+        </div>
+        <div id="position">
+            <h1>Junior Full Stack Engineer</h1>
+        </div>
         </>
     )
 }
