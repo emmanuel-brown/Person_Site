7cfdc173b62c23527866300a8aad76ab847ff65a
Emmanuel Oklo Brown <emmanuelbrown1310@gmail.com>
2019-10-09 09:24:06 -0400

    added everything

--- src/Components/Home/Headshot.jsx
+++ src/Components/Home/Headshot.jsx
@@ -2,13 +2,20 @@ import React from 'react'
 import HeadShot from './headShot1.jpeg'
 
 const Headshot = () =>{
+    const position = "Junior Web Developer".split("")
+
+    const letters = position.map((letter, i) =>{
+        return i%2 === 0 ? <span className="a">{letter}</span> :
+            <span className="b">{letter}</span>
+    })
+
     return(
         <>
         <div id="headShot">
             <img src={ HeadShot } alt=""/>
         </div>
         <div id="position">
-            <h1>Junior Full Stack Engineer</h1>
+            <h1>{ letters }</h1>
         </div>
         </>
     )
