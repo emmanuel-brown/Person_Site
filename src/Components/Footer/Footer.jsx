eda7d9488525455d91cab735a6deb5deead0f2a1
Emmanuel Oklo Brown <emmanuelbrown1310@gmail.com>
2019-11-18 12:26:27 -0500

    fixed footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () =>{
             <Box name="Github" username="Emmanuel-brown" link="https://github.com/" animation={true} isLeft={true} imageSize="100em" logo="https://icon-library.net/images/github-icon-png/github-icon-png-29.jpg"/>
             <Box name="LinkedIn" username="Emmanuel Brown" link="https://www.linkedin.com/in/emmanuel-brown-1a39b8186" isLeft={false} imageSize="100em"  logo="https://www.freepnglogos.com/uploads/linkedin-blue-style-logo-png-0.png"/>
             <Box name="Email" username="EmmanuelBrown1310 @gmail.com" link={`mailto: emmanuelbrown1310@gmail.com`} imageSize="100em" isLeft={true} logo="http://www.iconarchive.com/download/i98301/dakirby309/simply-styled/Gmail.ico" />
-            <Box name="Number" username="803-579-8542" isLeft={false} imageSize="150em" logo="https://www.shareicon.net/download/2017/05/06/885605_post.ico"/>
+            <Box name="Number" username="803-579-8542" isLeft={false} imageSize="150em" logo="https://icon-icons.com/icons2/1793/PNG/128/hashtag_114741.png"/>
             <div id="clicked">
                 <p className="press">Press!!</p>
                 <p className="click">Click!!</p>
